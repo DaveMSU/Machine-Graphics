c8d641fb341843e1c1e15fcb830dd4fbecd65e4c
DaveMSU <50180326+DaveMSU@users.noreply.github.com>
2020-04-06 01:45:57 +0500

    Add files via upload

---
+++ 01/Solution0.83/geometry.h
@@ -0,0 +1,34 @@
+#ifndef GEOM
+#define GEOM
+
+#include <vector>
+
+class Vec3f{
+        float rgb[3];
+public:
+        Vec3f();
+        Vec3f( float red, float green, float blue );
+        std::vector <float> get_rgb();
+        float& operator[]( size_t index );
+        const float& operator[]( size_t index ) const;
+        Vec3f  operator+( const Vec3f& right ) const;
+        Vec3f  operator+( const float number ) const;
+        Vec3f  operator-( const Vec3f& right ) const;
+        Vec3f  operator-( const float number ) const;
+        float  operator*( const Vec3f& right ) const;
+        Vec3f  operator*( const float number ) const;
+        Vec3f  operator/( const float number ) const;
+        Vec3f& operator*=( const float number );
+        Vec3f& operator/=( const float number );
+	Vec3f& operator+=( const Vec3f& right );
+	Vec3f& operator+=( const float number );
+	Vec3f& operator-=( const Vec3f& right );
+	Vec3f& operator-=( const float number );
+	Vec3f  operator-() const; // Унарный.
+	bool   operator==( const Vec3f& right ) const;
+        Vec3f normalize() const;
+	float length() const;
+        ~Vec3f();
+};
+
+#endif
